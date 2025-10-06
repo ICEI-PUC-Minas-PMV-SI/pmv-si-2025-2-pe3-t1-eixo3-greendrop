45e8abb02c4fb15ebde817b9d107984ec0cdcfdf
Matheus Bon <matheusbon29@gmail.com>
2025-10-06 20:39:37 -0300

    fix: update static file path and enhance login page layout

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
-app.use(express.static('src/public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home');
