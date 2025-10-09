82d64034eacf3fab4c00e03619c37712429749e0
Marcos Martins <marcos.santos@grupofigueiredo.com.br>
2025-10-09 10:17:38 -0300

    feat(pages): add initial static page structure

--- src/index.js
+++ src/index.js
@@ -15,6 +15,18 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/password-reset', (req, res) => {
+    res.render('password-reset');
+});
+
+app.get('/main-page', (req, res) => {
+    res.render('main-page');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
